dc0309d3c3418514b9896d212b92bbdc5ed505e2
Blake Hadland <blake.hadland@gmail.com>
2026-02-01 12:16:32 -0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+            serif: ['Merriweather', 'ui-serif', 'Georgia'],
+            mono: ['Fira Code', 'ui-monospace', 'monospace'],
+            saira: ['Saira', 'ui-sans-serif', 'system-ui'],
+            
+          },
+        },
+      },
+    plugins: [],
+  }
